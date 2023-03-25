ec504331c53743558a06c324a4e40929054d6e09
KibobiShtrudelz <petar.georgiev.kolev@gmail.com>
2023-03-25 16:50:23 +0200

    todo app with xstate

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,74 +1,13 @@
-import { send, createMachine } from 'xstate'
-import { createActorContext } from '@xstate/react'
-
 import { HamburgerMenu } from '@components'
 
 import styles from './header.module.scss'
 
-interface InitialState {
-  authorized: boolean
-  unauthorized: boolean
-}
-
-interface AuthEvents {
-  type: string
-}
-
-const authMachine = createMachine<InitialState, AuthEvents>({
-  // schema: {
-  //   events: {} as { type: 'LOG_IN' } | { type: 'LOG_OUT' }
-  // },
-
-  initial: 'unauthorized',
-
-  states: {
-    authorized: {
-      on: {
-        LOG_OUT: {
-          target: 'unauthorized'
-        }
-      }
-    },
-
-    unauthorized: {
-      on: {
-        LOG_IN: {
-          cond: 'authCheck',
-          target: 'authorized'
-        }
-      }
-    }
-  }
-})
-
-const AuthMachineContext = createActorContext(authMachine)
-
 export function Header(): JSX.Element {
   return (
-    <AuthMachineContext.Provider>
-      <header className={styles.header}>
-        <span className={styles.brand}>Snuki Computers</span>
+    <header className={styles.header}>
+      <span className={styles.brand}>Snuki Computers</span>
 
-        <button
-          type="button"
-          onClick={() => {
-            send('LOG_IN')
-          }}
-        >
-          LOG IN
-        </button>
-
-        <button
-          type="button"
-          onClick={() => {
-            send('LOG_OUT')
-          }}
-        >
-          LOG OUT
-        </button>
-
-        <HamburgerMenu />
-      </header>
-    </AuthMachineContext.Provider>
+      <HamburgerMenu />
+    </header>
   )
 }
--- src/pages/xstate-page/xstate.page.tsx
+++ src/pages/xstate-page/xstate.page.tsx
@@ -1,3 +1,97 @@
+import { useMachine } from '@xstate/react'
+import { todosMachine } from 'machines/todo-app-machine'
+
+import styles from './xstate-page.module.scss'
+
+const todos = new Set<string>([])
+
 export function XState(): JSX.Element {
-  return <h1>XSTATE</h1>
+  const [state, send] = useMachine(todosMachine, {
+    services: {
+      async loadTodos() {
+        return Array.from(todos)
+      },
+
+      async saveTodo(context, _event) {
+        todos.add(context.createNewTodoFormInputValue)
+      },
+
+      async deleteTodo(_context, event) {
+        todos.delete(event.todo)
+      }
+    }
+  })
+
+  return (
+    <div className={styles.xstate}>
+      <pre>{JSON.stringify(state.value)}</pre>
+
+      <pre>{JSON.stringify(state.context)}</pre>
+
+      <div>
+        {state.matches('todosLoaded') && (
+          <>
+            {state.context.todos.map(todo => (
+              <div key={todo} style={{ display: 'flex', alignItems: 'center' }}>
+                <p>{todo}</p>
+                <button
+                  onClick={e => {
+                    send({ type: 'DELETE_TODO', todo })
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+
+      <pre>
+        {state.matches('todosLoaded') && (
+          <button
+            type="button"
+            onClick={() => {
+              send({ type: 'CREATE_TODO' })
+            }}
+          >
+            Create TODO
+          </button>
+        )}
+      </pre>
+
+      {state.matches('deleteErroredTodo') && (
+        <>
+          <p>Something went wrong: {state.context.errorMessage}</p>
+          <button
+            type="button"
+            onClick={() => {
+              send({ type: 'SPEED_UP' })
+            }}
+          >
+            Go back to list
+          </button>
+        </>
+      )}
+
+      {state.matches('createTodo.showFormInput') && (
+        <form
+          onSubmit={e => {
+            e.preventDefault()
+            send({ type: 'SUBMIT' })
+          }}
+        >
+          <input
+            type="text"
+            onChange={e => {
+              send({
+                type: 'FORM_INPUT_CHANGE',
+                value: e.target.value
+              })
+            }}
+          />
+        </form>
+      )}
+    </div>
+  )
 }
